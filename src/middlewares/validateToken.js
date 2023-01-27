ee23cb022c93098ff93b97fe8699774456423024
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-01-26 22:00:52 -0300

    feat: criando middleware de análise do token

---
+++ src/middlewares/validateToken.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const TOKEN_SECRET = process.env.JWT_SECRET;
+
+const analyzeToken = (req, res, next) => {
+  const { authorization: token } = req.headers;
+  if (!token) return res.status(401).json({ message: 'Token not found' });
+  try {
+    const decoded = jwt.verify(token, TOKEN_SECRET);
+    res.locals.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = {
+  analyzeToken,
+};
